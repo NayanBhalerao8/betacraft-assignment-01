44466ea5f9e9bbf9a3cc187f5251f9be2dee9c35
NayanBhalerao8 <youremail@example.com>
2024-11-11 14:19:16 +0530

    tasks controller refactor

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,13 +1,12 @@
 module Api
   module V1
     class TasksController < ApplicationController
-      before_action :authenticate_user!  # Ensures user is authenticated
+      before_action :authenticate_user!
 
       def create
-        @project = Project.find(params[:project_id])
-        @task = @project.tasks.new(task_params.merge(user_id: current_user.id))  # Automatically set user_id from current_user
+        @task = Task.create_for_project_and_user(params[:project_id], current_user, task_params)
     
-        if @task.save
+        if @task
           render json: @task, status: :created
         else
           render json: @task.errors, status: :unprocessable_entity
@@ -15,15 +14,11 @@ module Api
       end
       
       def update
-        @project = Project.find_by(id: params[:project_id])  # Find project by ID
-        @task = @project.tasks.find_by(id: params[:id])       # Find task by ID within the project
+        @task = Task.find_in_project(params[:id], params[:project_id])
     
         if @task.nil?
           render json: { error: 'Task not found' }, status: :not_found
-          return
-        end
-    
-        if @task.update(task_params)
+        elsif @task.update(task_params)
           render json: @task, status: :ok
         else
           render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
@@ -33,7 +28,7 @@ module Api
       private
 
       def task_params
-        params.require(:task).permit(:completed, :title, :description)  # Adjust fields as necessary
+        params.require(:task).permit(:completed, :title, :description)
       end
     end
   end
