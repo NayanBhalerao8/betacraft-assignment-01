ee0bc764e8c5beccef1c384ec6826827d1fee8f7
NayanBhalerao8 <youremail@example.com>
2024-11-11 14:10:25 +0530

    projects controllar refactor

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -7,20 +7,22 @@ module Api
         @projects = current_user.projects
         render json: @projects, status: :ok
       end
-    
+
       def show
-        @project = current_user.projects.includes(tasks: :comments).find(params[:id])
-        render json: @project.to_json(include: { tasks: { include: :comments } })
+        @project = current_user.projects.find_by(id: params[:id])
+        if @project
+          render json: @project.to_json(include: { tasks: { include: :comments } })
+        else
+          render json: { error: 'Project not found' }, status: :not_found
+        end
       end
 
       def create
         @project = current_user.projects.build(project_params)
 
         if @project.save
-          # Render the newly created project as JSON
           render json: @project, status: :created
         else
-          # Return errors with 422 status if validation fails
           render json: { errors: @project.errors.full_messages }, status: :unprocessable_entity
         end
       end
